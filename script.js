ba5e5149141e69d7bb0851b0b0ef9f17dcb1dc9f
abhi2428063 <42578974+abhi2428063@users.noreply.github.com>
2023-12-26 08:55:54 +0000

    Initial commit with base files.

---
+++ script.js
@@ -0,0 +1,14 @@
+// Add your scripts here
+document.getElementById('write-btn').addEventListener('click', function() {
+    // Implement write button behavior
+});
+
+document.getElementById('new-btn').addEventListener('click', function() {
+    // Implement new button behavior
+});
+
+document.getElementById('menu-btn').addEventListener('click', function() {
+    // Implement menu button behavior
+});
+
+// Add more scripts as you need
